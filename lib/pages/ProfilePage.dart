40fe0f8c54e02e1a4736849a61ccab5341fbd124
kurakin101 <37266612+kurakin101@users.noreply.github.com>
2021-09-18 14:38:51 +0300

    fix

--- lib/pages/ProfilePage.dart
+++ lib/pages/ProfilePage.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:zaton/main.dart';
+import 'package:zaton/widgets/list_cart_portaint.dart';
 
 class ProfilePage extends StatefulWidget {
-
-
   @override
   State<StatefulWidget> createState() {
     return _ProfilePageState();
@@ -10,89 +12,171 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-
-
+  bool turn = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Text("ProfilePage"),
-      ),
-    );
-  }
-
-  Widget cardUi(String id, String ownerId, String image, String description, String date,
-      String price, String name, String category) {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-      // padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-
-      child: InkWell(
-        // onTap: () => Navigator.push(
-        //   context,
-        //   MaterialPageRoute(
-        //       builder: (context) => detailsPage(
-        //           id, image, description, date, name, price, category, ownerId, context)),
-        // ),
-        child: Container(
-          padding: EdgeInsets.all(8),
-          child: Stack(
-            children: [
-              Align(
-                alignment: Alignment.topCenter,
-                child: Container(
-                  padding: EdgeInsets.only(
-                    bottom: 32,
-                  ),
-                  // child: Neumorphic(
-                  //   style: NeumorphicStyle(
-                  //     intensity: 0,
-                  //     boxShape: NeumorphicBoxShape.roundRect(
-                  //         BorderRadius.circular(12)),
-                  //   ),
-                  //   child: Container(
-                  //     height: double.infinity,
-                  //     width: double.infinity,
-                  //     child: Image.network(image, fit: BoxFit.fill),
-                  //   ),
-                  // ),
+      backgroundColor: Color(0xffE5FFE8),
+      // appBar: AppBar(
+      //   elevation: 0,
+      //   backgroundColor: Color(0xffE5FFE8),
+      //   title: Row(
+      //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //     crossAxisAlignment: CrossAxisAlignment.center,
+      //     children: [
+      //       Icon(
+      //         Icons.arrow_back_outlined,
+      //         color: Color(0xff24C273),
+      //       ),
+      //       Text("Корзина",
+      //           style: GoogleFonts.montserrat(
+      //               fontSize: 28,
+      //               fontWeight: FontWeight.bold,
+      //               color: Color(0xff24C273))),
+      //       InkWell(
+      //         onTap: () => setState(() {
+      //           turn = !turn;
+      //         }),
+      //         child: Icon(
+      //           Icons.remove_shopping_cart_outlined,
+      //           color: Color(0xff24C273),
+      //         ),
+      //       ),
+      //     ],
+      //   ),
+      // ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.account_circle,
+              size: 110,
+            ),
+            Column(
+              children: [
+                Text(
+                  "Войдите в профиль",
+                  style: GoogleFonts.montserrat(
+                      fontSize: 26, fontWeight: FontWeight.w800),
                 ),
-              ),
-              Container(
-                // padding: EdgeInsets.symmetric(
-                //   horizontal: 16,
-                // ),
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        flex: 3,
-                        child: Text(
-                          name,
-                          style: TextStyle(
-                            fontWeight: FontWeight.w800,
-                            fontSize: 16,
-                          ),
-                        ),
+                SizedBox(
+                  height: 12,
+                ),
+                Text(
+                  "Начните покупки прямо сейчас",
+                  style: GoogleFonts.montserrat(fontSize: 16),
+                ),
+              ],
+            ),
+
+            Column(
+              children: [
+                Padding(padding: EdgeInsets.symmetric(horizontal: 30),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xff24C273),
+                      onPrimary: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
                       ),
-                      Expanded(
-                        child: Text(
-                          r"$" '$price',
-                          style: TextStyle(
-                            fontWeight: FontWeight.w800,
-                            fontSize: 16,
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 16, horizontal: 0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Войти",
+                            style: GoogleFonts.montserrat(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w800,
+                                color: Colors.white),
                           ),
-                        ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(padding: EdgeInsets.symmetric(horizontal: 30),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xffE5FFE8),
+                      onPrimary: Colors.white,
+                      shadowColor: Color(0xff24C273),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 16, horizontal: 0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Зарегистрироваться",
+                            style: GoogleFonts.montserrat(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w800,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Text(
+              "После регистрации вам будут доступны все возможности приложения",
+              textAlign: TextAlign.center,
+              style: GoogleFonts.montserrat(fontSize: 16),
+            ),
+            ),
+
+            Padding(padding: EdgeInsets.symmetric(horizontal: 30),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.white,
+                  onPrimary: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 16, horizontal: 0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(Icons.info_outlined, color: Color(0xff24C273),),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text(
+                        "Служба подде",
+                        style: GoogleFonts.montserrat(
+                            fontSize: 14,
+                            color: Colors.black),
                       ),
                     ],
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
