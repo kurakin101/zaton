f47b567da5436babf5228ad560fe4134e4a82acf
Ｖａｓｉｌｉｙ <37266612+kurakin101@users.noreply.github.com>
2021-09-18 20:49:54 +0300

    22

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-<<<<<<< HEAD
 import 'package:zaton/pages/ProfileSettingsPage.dart';
-=======
 import 'package:hexcolor/hexcolor.dart';
 import 'package:zaton/main.dart';
 import 'package:zaton/widgets/list_cart_portaint.dart';
->>>>>>> origin/main
 
 class LoginPage extends StatefulWidget {
   @override
@@ -22,7 +19,6 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-<<<<<<< HEAD
       backgroundColor: Color(0xffE5FFE8),
       appBar: AppBar(
         centerTitle: true,
@@ -117,25 +113,24 @@ class _LoginPageState extends State<LoginPage> {
           ),
         ),
       ),
-=======
       //backgroundColor: Color(0xffE5FFE8),
-      appBar: AppBar(
-        centerTitle: true,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: HexColor('#24C273'),),
-          onPressed: () {},
-        ),
-        title: Text(
-          'Вход',
-          style: GoogleFonts.montserrat(
-              color: HexColor('#24C273'),
-              fontSize: 28,
-              fontWeight: FontWeight.bold
-          ),
-        ),
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
+      // appBar: AppBar(
+      //   centerTitle: true,
+      //   leading: IconButton(
+      //     icon: Icon(Icons.arrow_back, color: HexColor('#24C273'),),
+      //     onPressed: () {},
+      //   ),
+      //   title: Text(
+      //     'Вход',
+      //     style: GoogleFonts.montserrat(
+      //         color: HexColor('#24C273'),
+      //         fontSize: 28,
+      //         fontWeight: FontWeight.bold
+      //     ),
+      //   ),
+      //   backgroundColor: Colors.transparent,
+      //   elevation: 0,
+      // ),
       // appBar: AppBar(
       //   elevation: 0,
       //   backgroundColor: Color(0xffE5FFE8),
@@ -164,77 +159,6 @@ class _LoginPageState extends State<LoginPage> {
       //     ],
       //   ),
       // ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Container(
-                    color: Colors.black12,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          hintText: 'E-mail',
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 2,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Container(
-                    color: Colors.black12,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          hintText: 'Пароль',
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Padding(padding: EdgeInsets.symmetric(horizontal: 30),
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      primary: Color(0xff24C273),
-                      onPrimary: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15.0),
-                      ),
-                    ),
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(vertical: 16, horizontal: 0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            "Войти",
-                            style: GoogleFonts.montserrat(
-                                fontSize: 18,
-                                fontWeight: FontWeight.w800,
-                                color: Colors.white),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-        ),
->>>>>>> origin/main
     );
   }
 }
